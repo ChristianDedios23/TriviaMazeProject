35ec165b27d9a49700e8aad4f37594f92430fb38
choppersbbk <krazyboykhalid@gmail.com>
2025-06-03 12:55:20 -0700

    Javadocs for question factory

--- IdeaProjects/TriviaMazeGame/src/Model/QuestionFactory.java
+++ IdeaProjects/TriviaMazeGame/src/Model/QuestionFactory.java
@@ -10,21 +10,47 @@ import java.util.*;
 
 import static Model.Enum.QuestionType.*;
 
+/**
+ * Question factory method to get questions.
+ */
 public class QuestionFactory {
-
+    /**
+     * Index tracker to keep track of the current question index.
+     */
     private static int mySACurrentIndex = 0;
+    /**
+     * Index tracker to keep track of the current question index.
+     */
     private static int myMCCurrentIndex = 0;
+    /**
+     * Index tracker to keep track of the current question index.
+     */
     private static int myTFCurrentIndex = 0;
 
+    /**
+     * List to store true and false questions.
+     */
     private static List<TrueAndFalseQuestion> myTFQuestionList = new ArrayList<>();
+    /**
+     * List to store multiple choice questions.
+     */
     private static List<MultipleChoiceQuestion> myMCQuestionList = new ArrayList<>();
+    /**
+     * List to store short answer questions.
+     */
     private static List<ShortAnswerQuestion> myShortAnswerQuestionList = new ArrayList<>();
 
+    /**
+     * Set that stores the type of questions currently active in the game.
+     */
     private static Set<QuestionType> myQuestionTypeSet = new HashSet<QuestionType>();
 
     private QuestionFactory() {
     }
 
+    /**
+     * Method to setup and initialize the question lists from the database.
+     */
     public static void setupQuestions() {
 
         String queryTF = "SELECT * FROM TRUE_OR_FALSE";
@@ -65,6 +91,12 @@ public class QuestionFactory {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Method gets a random question from the lists depending on what question types are available
+     * in the question type set and returns it.
+     * @return question A random question based on the settings of the game.
+     */
     public static AbstractQuestion getQuestion() {
         Random rand = new Random();
         String type = myQuestionTypeSet.toArray()[rand.nextInt(myQuestionTypeSet.size())].toString();
@@ -97,23 +129,32 @@ public class QuestionFactory {
     }
 
 
-    public static void editMyQuestionTypeSet(QuestionType questionType) {
-        if (myQuestionTypeSet.contains(questionType)) {
-            myQuestionTypeSet.remove(questionType);
+    /**
+     * Changes the selection of questions to choose from.
+     * @param theQuestionType the question type being added or removed from the set.
+     */
+    public static void editMyQuestionTypeSet(final QuestionType theQuestionType) {
+        if (myQuestionTypeSet.contains(theQuestionType)) {
+            myQuestionTypeSet.remove(theQuestionType);
         } else {
-            myQuestionTypeSet.add(questionType);
+            myQuestionTypeSet.add(theQuestionType);
         }
 
-        shuffleList(questionType);
+        shuffleList(theQuestionType);
     }
-    private static void shuffleList(final QuestionType questionType) {
-        if(questionType == SHORT_ANSWER) {
+
+    /**
+     * Shuffles the list after the questions have all been asked.
+     * @param theQuestionType
+     */
+    private static void shuffleList(final QuestionType theQuestionType) {
+        if(theQuestionType == SHORT_ANSWER) {
             Collections.shuffle(myShortAnswerQuestionList);
         }
-        else if(questionType == MULTIPLE_CHOICE) {
+        else if(theQuestionType == MULTIPLE_CHOICE) {
             Collections.shuffle(myMCQuestionList);
         }
-        else if(questionType == TRUE_OR_FALSE) {
+        else if(theQuestionType == TRUE_OR_FALSE) {
             Collections.shuffle(myTFQuestionList);
         }
     }
