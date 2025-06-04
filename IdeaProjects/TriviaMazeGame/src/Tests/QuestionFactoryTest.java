d8dd2509f495e399a20b10b127663059040b829e
choppersbbk <krazyboykhalid@gmail.com>
2025-06-03 21:59:55 -0700

    Java docs for question factory unit test

--- IdeaProjects/TriviaMazeGame/src/Tests/QuestionFactoryTest.java
+++ IdeaProjects/TriviaMazeGame/src/Tests/QuestionFactoryTest.java
@@ -7,9 +7,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit Test for question factory questions.
+ */
 public class QuestionFactoryTest {
 
     @BeforeAll
+    /**
+     * Sets up the questionFactory to contain all types of questions.
+     */
     static void setUpBeforeClass() throws Exception {
         QuestionFactory.setupQuestions();
         if(!QuestionFactory.checkQuestionTypes().contains(QuestionType.SHORT_ANSWER)) {
@@ -23,6 +29,9 @@ public class QuestionFactoryTest {
         }
     }
     @Test
+    /**
+     * Tests 100 random questions to see if they are made.
+     */
     public void testCreateQuestion() {
         AbstractQuestion question = QuestionFactory.getQuestion();
         for(int i=0; i<100; i++) {
@@ -32,6 +41,9 @@ public class QuestionFactoryTest {
     }
 
     @Test
+    /**
+     * Tests to see if when all questions of a certain type are asked, it reshuffles the questions.
+     */
     public void testShuffleQuestion() {
         QuestionFactory.editMyQuestionTypeSet(QuestionType.SHORT_ANSWER);
         QuestionFactory.editMyQuestionTypeSet(QuestionType.TRUE_OR_FALSE);
